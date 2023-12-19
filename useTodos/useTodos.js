3a88b9627a3176ac5a67e0434e1ffc6befff7373
freinavn <153737426+freinavn@users.noreply.github.com>
2023-12-19 21:15:49 +0100

    Add files via upload

---
+++ useTodos/useTodos.js
@@ -0,0 +1,48 @@
+// const { todos, handleDeleteTodo, handleToggleTodo, handleNewTodo} = useTodos();
+
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "./todoReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodos = () => {
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
+    };
+
+    dispatch(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: "[TODO] Remove Todo",
+      payload: id,
+    });
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatch({
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    });
+  };
+
+  return {
+    todosCount: todos.length,
+    todosPendingCount: todos.filter((todo) => !todo.done).length,
+    todos,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+  };
+};
